e1e9b89c86d10e176f9a17828c95f2366e5b2459
Vegard Gillestad <vegard.gillestad@gmail.com>
2020-08-29 20:38:25 +0200

    initial

---
+++ hunttracker-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hunttracker-ios
+//
+//  Created by Vegard Gillestad on 29/08/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
