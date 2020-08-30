7b870ee3acf7d5fb759d68728a0a9f857bd2b456
Vegard Gillestad <vegard.gillestad@gmail.com>
2020-08-30 20:29:30 +0200

    navigator + app icons

--- hunttracker-ios/SceneDelegate.swift
+++ hunttracker-ios/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -17,6 +18,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+//        let a = CLLocationManager()
+//        a.requestWhenInUseAuthorization()
         
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = WebViewController() // Your initial view controller.
--- hunttracker-ios/ViewControllers/WebViewController.swift
+++ hunttracker-ios/ViewControllers/WebViewController.swift
@@ -11,11 +11,19 @@ import WebKit
 
 class WebViewController : UIViewController {
     
-    private let webView = WKWebView()
+    private var webView:WKWebView!
     private let activityView = UIActivityIndicatorView()
+    
+    private let navigatorGeolocation = NavigatorGeolocation();
     
     override func viewDidLoad() {
         view.backgroundColor = UIColor.green
+        
+        let webViewConfiguration = WKWebViewConfiguration();
+        navigatorGeolocation.setUserContentController(webViewConfiguration: webViewConfiguration);
+        webView = WKWebView(frame:.zero , configuration: webViewConfiguration);
+        webView.navigationDelegate = self;
+        navigatorGeolocation.setWebView(webView: webView);
         
         webView.backgroundColor = UIColor.purple
         webView.frame = view.bounds
@@ -34,10 +42,10 @@ class WebViewController : UIViewController {
     }
 }
 
-
-
 extension WebViewController : WKNavigationDelegate {
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         activityView.stopAnimating()
+        
+        webView.evaluateJavaScript(navigatorGeolocation.getJavaScripToEvaluate());
     }
 }
