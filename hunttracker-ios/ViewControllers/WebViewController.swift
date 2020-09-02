62bb15e3467ca27490fd2163e0dde6f8bd9e00c3
Vegard Gillestad <vegard.gillestad@gmail.com>
2020-09-02 20:53:25 +0200

    minor

--- hunttracker-ios/ViewControllers/WebViewController.swift
+++ hunttracker-ios/ViewControllers/WebViewController.swift
@@ -11,26 +11,24 @@ import WebKit
 
 class WebViewController : UIViewController {
     
+    static let BASE_URL = "https://hunttracker.herokuapp.com"
+    
     private var webView:WKWebView!
     private let activityView = UIActivityIndicatorView()
     
     private let navigatorGeolocation = NavigatorGeolocation();
     
     override func viewDidLoad() {
-        view.backgroundColor = UIColor.green
+        view.backgroundColor = UIColor.white
         
         let webViewConfiguration = WKWebViewConfiguration();
-        navigatorGeolocation.setUserContentController(webViewConfiguration: webViewConfiguration);
         webView = WKWebView(frame:.zero , configuration: webViewConfiguration);
         webView.navigationDelegate = self;
+        webView.load(URLRequest(url: URL(string: WebViewController.BASE_URL)!))
+        
+        navigatorGeolocation.setUserContentController(webViewConfiguration: webViewConfiguration);
         navigatorGeolocation.setWebView(webView: webView);
         
-        webView.backgroundColor = UIColor.purple
-        webView.frame = view.bounds
-        webView.navigationDelegate = self
-        webView.load(URLRequest(url: URL(string: "https://hunttracker.herokuapp.com")!))
-        
-        activityView.center = view.center
         activityView.startAnimating()
         
         view.addSubview(webView)
@@ -39,6 +37,13 @@ class WebViewController : UIViewController {
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .darkContent
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        webView.frame = view.bounds
+        activityView.center = view.center
     }
 }
 
